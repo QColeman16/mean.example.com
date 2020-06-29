137cde848f2adf11dc0142b96f6f66d84352f5ac
Qudamah Coleman <cfcinc.88#gmail.com>
2020-06-29 10:56:42 -0500

    go

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,9 @@
-var express = require('express');
+var express = require('Express');
 var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send('layout.pug');
 });
 
 module.exports = router;
