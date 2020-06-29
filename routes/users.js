c547c72cc19c756ca58324ca257f992f71ef3a3e
Qudamah Coleman <cfcinc.88#gmail.com>
2020-06-29 11:02:39 -0500

    Setup ExpressJS with Pug

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,9 @@
-var express = require('Express');
+var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('layout.pug');
+  res.send('respond with a resource');
 });
 
 module.exports = router;
